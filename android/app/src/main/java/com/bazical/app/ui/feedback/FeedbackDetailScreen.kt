7305325bae2c336304064328579d5680a9b4de8c
jack <qinshenyong@socialdatamax.com>
2026-05-22 18:50:18 +0800

    fix anzhuo

--- android/app/src/main/java/com/bazical/app/ui/feedback/FeedbackDetailScreen.kt
+++ android/app/src/main/java/com/bazical/app/ui/feedback/FeedbackDetailScreen.kt
@@ -28,6 +28,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
