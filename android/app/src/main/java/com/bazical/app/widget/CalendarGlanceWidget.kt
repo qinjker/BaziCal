c18b2e7ddf001af9ee320b52cd1ece4add7099ff
jack <qinshenyong@socialdatamax.com>
2026-05-27 18:10:49 +0800

    fix anzhuo Engine error

--- android/app/src/main/java/com/bazical/app/widget/CalendarGlanceWidget.kt
+++ android/app/src/main/java/com/bazical/app/widget/CalendarGlanceWidget.kt
@@ -7,6 +7,7 @@ import androidx.glance.GlanceId
 import androidx.glance.GlanceModifier
 import androidx.glance.GlanceTheme
 import androidx.glance.appwidget.GlanceAppWidget
+import androidx.glance.appwidget.GlanceAppWidgetReceiver
 import androidx.glance.appwidget.provideContent
 import androidx.glance.background
 import androidx.glance.column
